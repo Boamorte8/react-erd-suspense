9a3ecf9bd1db6047f3c131355eab2b37b0784601
Esteban Salazar <esteban.salazar@gorillalogic.com>
2022-12-15 17:56:53 -0500

    finish to work in exercise 01

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -2,36 +2,77 @@
 // http://localhost:3000/isolated/exercise/01.js
 
 import * as React from 'react'
-// 🐨 you'll also need to get the fetchPokemon function from ../pokemon:
-import {PokemonDataView} from '../pokemon'
+import {createResource} from 'utils'
+import {
+  fetchPokemon,
+  PokemonDataView,
+  PokemonErrorBoundary,
+  PokemonInfoFallback,
+} from '../pokemon'
 
-// 💰 use it like this: fetchPokemon(pokemonName).then(handleSuccess, handleFailure)
+// function createResource(promise) {
+//   let status = 'pending'
+//   let result = promise.then(
+//     resolved => {
+//       status = 'resolved'
+//       result = resolved
+//     },
+//     rejected => {
+//       status = 'rejected'
+//       result = rejected
+//     },
+//   )
+//   return {
+//     read() {
+//       if (status === 'pending' || status === 'rejected') throw result
+//       if (status === 'resolved') return result
+//     },
+//   }
+// }
 
-// 🐨 create a variable called "pokemon" (using let)
+// let pokemon
+// let pokemonError
 
-// 💣 delete this now...
-const pokemon = {
-  name: 'TODO',
-  number: 'TODO',
-  attacks: {
-    special: [{name: 'TODO', type: 'TODO', damage: 'TODO'}],
-  },
-  fetchedAt: 'TODO',
-}
+// class PokemonErrorBoundary extends React.Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {hasError: false}
+//   }
+
+//   static getDerivedStateFromError(error) {
+//     // Update state so the next render will show the fallback UI.
+//     return {hasError: true}
+//   }
+//   render() {
+//     if (this.state.hasError) {
+//       // You can render any custom fallback UI
+//       return <h1>Error loading pokemon. Try again</h1>
+//     }
+//     return this.props.children
+//   }
+// }
 
 // We don't need the app to be mounted to know that we want to fetch the pokemon
 // named "pikachu" so we can go ahead and do that right here.
-// 🐨 assign a pokemonPromise variable to a call to fetchPokemon('pikachu')
+// const pokemonPromise = fetchPokemon('pichurria')
 
-// 🐨 when the promise resolves, assign the "pokemon" variable to the resolved value
-// 💰 For example: somePromise.then(resolvedValue => (someValue = resolvedValue))
+//   pokemonPromise.then(
+//     pokemonData => (pokemon = pokemonData),
+//     error => (pokemonError = error),
+//   )
+
+const pokemonResource = createResource(fetchPokemon('pikacha'))
 
 function PokemonInfo() {
-  // 🐨 if there's no pokemon yet, then throw the pokemonPromise
-  // 💰 (no, for real. Like: `throw pokemonPromise`)
-
   // if the code gets it this far, then the pokemon variable is defined and
   // rendering can continue!
+  // if (pokemonError) {
+  //   throw pokemonError
+  // }
+  // if (!pokemon) {
+  //   throw pokemonPromise
+  // }
+  const pokemon = pokemonResource.read()
   return (
     <div>
       <div className="pokemon-info__img-wrapper">
@@ -46,8 +87,12 @@ function App() {
   return (
     <div className="pokemon-info-app">
       <div className="pokemon-info">
-        {/* 🐨 Wrap the PokemonInfo component with a React.Suspense component with a fallback */}
-        <PokemonInfo />
+        <PokemonErrorBoundary>
+          {/* <React.Suspense fallback={<div>Loading pokemon...</div>}> */}
+          <React.Suspense fallback={<PokemonInfoFallback />}>
+            <PokemonInfo />
+          </React.Suspense>
+        </PokemonErrorBoundary>
       </div>
     </div>
   )
